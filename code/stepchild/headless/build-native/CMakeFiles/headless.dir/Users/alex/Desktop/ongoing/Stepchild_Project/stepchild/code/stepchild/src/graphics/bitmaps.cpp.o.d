a975a5afbb9b03798280bc98e111eee44f49ef46
alexlafetra <alexlafetrathompson@gmail.com>
2026-08-05 03:03:10 -0400

    gh actions test

--- code/stepchild/headless/build-native/CMakeFiles/headless.dir/Users/alex/Desktop/ongoing/Stepchild_Project/stepchild/code/stepchild/src/graphics/bitmaps.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/headless.dir/Users/alex/Desktop/ongoing/Stepchild_Project/stepchild/code/stepchild/src/graphics/bitmaps.cpp.o: \
-  /Users/alex/Desktop/ongoing/Stepchild\ Project/stepchild/code/stepchild/src/graphics/bitmaps.cpp
--- code/stepchild/headless/build-native/CMakeFiles/headless.dir/Users/alex/Desktop/ongoing/Stepchild_Project/stepchild/code/stepchild/src/menus/fragmentMenu.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/headless.dir/Users/alex/Desktop/ongoing/Stepchild_Project/stepchild/code/stepchild/src/menus/fragmentMenu.cpp.o: \
-  /Users/alex/Desktop/ongoing/Stepchild\ Project/stepchild/code/stepchild/src/menus/fragmentMenu.cpp
